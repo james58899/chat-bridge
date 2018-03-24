db75fafd9e174975c4af7f753b5a44492e6e8ac8
james58899 <james59988@gmail.com>
2018-03-25 03:31:27 +0800

    remove cli

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const path = require('path');
 const fs = require('fs');
 const EventEmitter = require('events');
-const readline = require('readline');
-const rl = readline.createInterface(process.stdin, process.stdout);
 const ignore = require(path.resolve('data', 'ignore.json'));
 
 class Emitter extends EventEmitter {}
@@ -25,40 +23,6 @@ const checkIgnore = function(nick) {
     });
     return result;
 };
-
-// CLI
-rl.setPrompt('> ');
-rl.prompt();
-rl.on('line', (line) => {
-    line = line.trim().split(' ');
-    switch (line[0]) {
-    case 'ban':
-        if (line.length === 2 && ignore.indexOf(line[1]) < 0) {
-            ignore.push(line[1]);
-            fs.writeFile(path.resolve('data', 'ignrore.json'), JSON.stringify(ignore), function(err) {
-                if (err) {
-                    throw err;
-                }
-                console.log('已封鎖 ' + line[1]);
-            });
-        } else {
-            console.log('對象無效或已在忽略名單中');
-        }
-        break;
-    case 'unban':
-        if (line.length === 2 && ignore.indexOf(line[1]) > -1) {
-            ignore.splice(ignore.indexOf(line[1]), 1);
-            fs.writeFile(path.resolve('data', 'ignrore.json'), JSON.stringify(ignore), function(err) {
-                if (err) throw err;
-                console.log('已解除封鎖 ' + line[1]);
-            });
-        } else {
-            console.log('對象無效或未在忽略名單中');
-        }
-        break;
-    }
-    rl.prompt();
-}).on('close', () => process.exit(0));
 
 // Init Modules
 console.log('Loading modules...');
